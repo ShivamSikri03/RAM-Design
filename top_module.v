3a015041aceff750a020c8a015c0cb77bb22c72c
ShivamSikri03 <shivamsikri36@gmail.com>
2025-07-14 22:28:45 +0530

    Create top_module.v

---
+++ top_module.v
@@ -0,0 +1,20 @@
+module simple_sync_ram (
+    input wire clk,           
+    input wire we,            
+    input wire [3:0] addr,    
+    input wire [7:0] din,     
+    output reg [7:0] dout     
+);
+    
+    reg [7:0] mem [0:15];
+
+    always @(posedge clk) begin
+        if (we) begin            
+            mem[addr] <= din;
+        end 
+        else begin
+            dout <= mem[addr];
+        end
+    end
+
+endmodule
